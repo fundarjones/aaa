3c57d5355c376167ab78013ebd7c9e3e6248702f
fundarjones <62589874+fundarjones@users.noreply.github.com>
2020-09-08 22:15:24 +0800

    Add files via upload

---
+++ form.php
@@ -0,0 +1,15 @@
+<?php 
+	$to = "drumitarist@gmail.com";
+	$subject = "Music Studio Reservation";
+
+	$bname = $_POST['bname'];
+	$email = $_POST['email'];
+	$contact = $_POST['contact'];
+
+	$headers .= "Content-type: text/html;\r\n";
+	$headers .= "From: $contact";
+
+	mail($to, $subject, $bname, $contact, $headers);
+	echo "Your Reservation has been sent! Please wait for our response, thank you! $bname";
+
+?>
