190bd0cbeed8d3ec5ecf055698a57003748a31b5
fundarjones <62589874+fundarjones@users.noreply.github.com>
2020-09-08 22:03:35 +0800

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+</head>
+<body>
+	<form action="form.php" method="POST">
+		<p>Band Name:
+			<input name="bname" type="text" id="bname">
+		</p>
+		<p>E-mail:
+			<input type="text" name="email" id="email">
+		<p>Contact:
+			<input type="text" name="contact" id="contact">
+		</p>
+		<p>Submit
+			<input type="submit" name="submit" id="submit" value="Submit">
+		</p>
+	</form>
+</body>
+</html>
